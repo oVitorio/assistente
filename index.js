191f66f56f736010d6fcfb10a0d7506553ca3887
Replit user <>
2023-11-07 23:06:59 +0000

    dados mockados

--- index.js
+++ index.js
@@ -2,32 +2,42 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+const port = 3000;
+
 app.use(bodyParser.json());
 
-app.post('/', (req, res) => {
-    const requestBody = req.body;
+const dadosManutencoes = [
+    {"nome": "Manutenção do Motor", "tipo": "Preventiva", "tarefas_alocadas": 5},
+    {"nome": "Inspeção da Fuselagem", "tipo": "Corretiva", "tarefas_alocadas": 3},
+    {"nome": "Reparo do Trem de Pouso", "tipo": "Emergencial", "tarefas_alocadas": 2}
+    // Adicione mais manutenções conforme necessário
+];
 
-    // Extract action from the request body
-    const action = requestBody.queryResult.intent.displayName;
+app.post('/webhook', (req, res) => {
+    const intent = req.body.queryResult.intent.displayName;
 
-    switch (action) {
-        case 'ola':
-            // Code to execute for this intent
-            const fulfillmentText = 'Olá, mundo!';
-            const responseJson = {
-                fulfillmentText: fulfillmentText,
-                outputContexts: requestBody.queryResult.outputContexts,
-                source: 'webhookdata',
-            };
-            res.status(200).send(responseJson);
-            break;
-        default:
-            // Code to execute for unrecognized intent
-            console.log('Unknown Intent');
-            res.status(200).send({});
+    if (intent === 'ConsultaManutencoes') {
+        const response = consultarManutencoes();
+        res.json(response);
+    } else {
+        const response = {"fulfillmentText": "Desculpe, não entendi o seu pedido."};
+        res.json(response);
     }
 });
 
-app.listen(8080, () => {
-    console.log('Webhook service is listening on port 8080');
+function consultarManutencoes() {
+    // Lógica para consultar o banco de dados e obter informações sobre as manutenções
+    // Você pode acessar a lista dadosManutencoes aqui para obter os dados
+
+    // Exemplo de resposta com base nos dados do banco de dados
+    let resposta = "Aqui estão as manutenções agendadas para este mês:\n";
+    dadosManutencoes.forEach(manutencao => {
+        resposta += `Nome: ${manutencao.nome}, Tipo: ${manutencao.tipo}, Tarefas Alocadas: ${manutencao.tarefas_alocadas}\n`;
+    });
+
+    return {"fulfillmentText": resposta};
+}
+
+app.listen(port, () => {
+    console.log(`Servidor webhook está rodando na porta ${port}`);
 });
